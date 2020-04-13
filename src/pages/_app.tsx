8553e0d32f1bf2e8c2c543292f3c94c89653c469
Daniel Emod Kovacs <kovacsemod@gmail.com>
2020-04-13 19:38:54 +0200

    feat: set vh variable on app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,11 @@ import { AppContext, AppInitialProps, AppProps } from "next/app";
 import "typeface-nunito";
 import "typeface-poppins";
 
-const Container = styled.div`
+import useViewport from "../hooks/useViewport";
+
+const Container = styled.div<{ viewportHeight: number }>`
+  --vh: ${(props) => props.viewportHeight / 100}px;
+
   :global() {
     html {
       box-sizing: border-box;
@@ -42,8 +46,9 @@ const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
   Component,
   pageProps,
 }) => {
+  const { height } = useViewport();
   return (
-    <Container>
+    <Container viewportHeight={height}>
       <Component {...pageProps} />
     </Container>
   );
