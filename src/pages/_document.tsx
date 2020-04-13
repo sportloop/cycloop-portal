40d9873a6d2237a2a6622dbac98d70f7eb4cb4ca
Daniel Emod Kovacs <kovacsemod@gmail.com>
2020-04-13 14:01:09 +0200

    chore: update typing on document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,13 @@
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import NextDocument, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 
 class Document extends NextDocument {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await NextDocument.getInitialProps(ctx);
     return { ...initialProps };
   }
